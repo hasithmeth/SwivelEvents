eebdfa49e161e9f5df2ac406c368d69a84d39c0b
Hasith Methmal Jayasekara <hasithmethmal@gmail.com>
2024-12-06 13:32:21 +0530

    feat: add font definitions and update Info.plist for custom fonts

---
+++ src/@types/fonts.d.ts
@@ -0,0 +1,20 @@
+interface Fonts {
+  INTER_BLACK: string;
+  INTER_BOLD: string;
+  INTER_EXTRA_BOLD: string;
+  INTER_EXTRA_LIGHT: string;
+  INTER_LIGHT: string;
+  INTER_MEDIUM: string;
+  INTER_REGULAR: string;
+  INTER_SEMI_BOLD: string;
+  INTER_THIN: string;
+  NOTOSANS_BLACK: string;
+  NOTOSANS_BOLD: string;
+  NOTOSANS_EXTRA_BOLD: string;
+  NOTOSANS_EXTRA_LIGHT: string;
+  NOTOSANS_LIGHT: string;
+  NOTOSANS_MEDIUM: string;
+  NOTOSANS_REGULAR: string;
+  NOTOSANS_SEMI_BOLD: string;
+  NOTOSANS_THIN: string;
+}
--- src/screens/welcome/Login.tsx
+++ src/screens/welcome/Login.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { FONT } from '../../assets/fonts';
 
 const Login: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text
+        style={{
+          fontSize: 24,
+          fontFamily: FONT.INTER_EXTRA_LIGHT,
+        }}>
+        Login
+      </Text>
     </View>
   );
 };
